b96eb8cd9b686cfea552117adf3d7c7404d3bc56
Savio Martin <saviomartin2007@gmail.com>
2021-02-15 11:30:06 +0530

    back to normal

--- src/components/output/Output.js
+++ src/components/output/Output.js
@@ -4,37 +4,9 @@ import { useParams } from "react-router-dom";
 const Output = () => {
   let params = useParams();
 
-  const github_data = {
-    token: "11d8b0f36ce132818cead3107f3b5d9db0784454",
-    username: "saviomartin",
-  };
-
   const githubUserAPI = "https://api.github.com/users/" + params.username;
   useEffect(() => {
-    fetch("https://api.github.com/graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authentication: "token 11d8b0f36ce132818cead3107f3b5d9db0784454",
-      },
-      body: JSON.stringify({
-        query: `
-    query {
-      user(login: "saviomartin") {
-        bio
-        avatarUrl
-        createdAt
-        name
-        websiteUrl
-        url
-        twitterUsername
-        isHireable
-        location
-      }
-    }
-    `,
-      }),
-    })
+    fetch(githubUserAPI)
       .then((res) => res.json())
       .then((data) => console.log(data));
   }, []);
