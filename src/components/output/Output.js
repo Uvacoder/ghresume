2c2cff30869542e14f8c23ff8642c5737ead3103
Savio Martin <saviomartin2007@gmail.com>
2021-02-15 12:18:13 +0530

    graphql api test

--- src/components/output/Output.js
+++ src/components/output/Output.js
@@ -4,11 +4,49 @@ import { useParams } from "react-router-dom";
 const Output = () => {
   let params = useParams();
 
+  const github_data = {
+    token: "11d8b0f36ce132818cead3107f3b5d9db0784454",
+    username: "saviomartin",
+  };
+
   const githubUserAPI = "https://api.github.com/users/" + params.username;
   useEffect(() => {
-    fetch(githubUserAPI)
+    fetch("https://api.github.com/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authentication: "token 11d8b0f36ce132818cead3107f3b5d9db0784454",
+      },
+      body: JSON.stringify({
+        query: `
+    query {
+      user(login: "saviomartin") {
+        bio
+        avatarUrl
+        createdAt
+        name
+        websiteUrl
+        url
+        twitterUsername
+        isHireable
+        location
+      }
+    }
+    `,
+      }),
+    })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        return (
+          <div className="output">
+            <div>
+              In React Router v5, You can use hooks to get parameters.
+              {data}
+            </div>
+          </div>
+        );
+      });
   }, []);
 
   return (
